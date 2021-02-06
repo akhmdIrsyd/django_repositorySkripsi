c14f983681a529814cb66ee3fdb87f328095d627
akhmad Irsyad <blank.irsyad@gmail.com>
2021-02-06 18:30:03 +0700

    forth commit

--- repasitory_fhut/settings.py
+++ repasitory_fhut/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w&-e@9(8pa8czq1*f1gp+ssr$gwz6@o2%&3==30b)d)qkkbedd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
